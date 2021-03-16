82b9084484009fb6db543d1271567447b93e50df
efeka <efekaraman@hotmail.com>
2021-03-16 17:30:00 +0300

    implemented game loop and the Game class which will handle all objects

---
+++ app/src/main/java/com/example/androidstudio_footballpong/Game.java
@@ -0,0 +1,96 @@
+package com.example.androidstudio_footballpong;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Manages all objects in the game by updating and rendering them.
+ */
+
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+
+    private GameLoop gameLoop;
+
+    private int touchPauseTimer = 0, touchPauseCooldown = 0;
+    private boolean touchPaused = false;
+
+    public Game(Context context) {
+        super(context);
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
+    }
+
+    public static enum STATE{
+        MAIN_MENU,
+        PAUSED_1P,
+        PAUSED_2P,
+        ONE_PLAYER,
+        TWO_PLAYERS
+    };
+    public static STATE state;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        if (gameLoop.getState().equals(Thread.State.TERMINATED)) {
+            SurfaceHolder surfaceHolder = getHolder();
+            surfaceHolder.addCallback(this);
+            gameLoop = new GameLoop(this, surfaceHolder);
+        }
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {}
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {}
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(getContext(), R.color.white);
+        paint.setColor(color);
+        paint.setTextSize(36);
+        canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(getContext(), R.color.white);
+        paint.setColor(color);
+        paint.setTextSize(36);
+        canvas.drawText("FPS: " + averageFPS, 100, 200, paint);
+    }
+
+    public void update() {
+
+    }
+
+    public void pauseGame() {
+        gameLoop.pauseLoop();
+    }
+
+}
--- app/src/main/java/com/example/androidstudio_footballpong/MainActivity.java
+++ app/src/main/java/com/example/androidstudio_footballpong/MainActivity.java
@@ -1,13 +1,47 @@
-package com.example.androidstudio_footballpong;
+package com.example.androidstudio_pingpong;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.androidstudio_footballpong.Game;
+
+//Entry point to the application
+
+public class MainActivity extends Activity {
+
+    public static int screenWidth, screenHeight;
+
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
+
+        game = new Game(this);
+        game.state = Game.STATE.MAIN_MENU;
+        setContentView(game);
     }
+
+    @Override
+    protected void onPause() {
+        game.pauseGame();
+        super.onPause();
+    }
+
 }
