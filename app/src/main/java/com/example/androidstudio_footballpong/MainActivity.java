593ea288e09b025c28c2e7eacaf39ebe48666d1b
efeka <efekaraman@hotmail.com>
2021-03-16 16:35:42 +0300

    initial commit

---
+++ app/src/main/java/com/example/androidstudio_footballpong/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidstudio_footballpong;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
