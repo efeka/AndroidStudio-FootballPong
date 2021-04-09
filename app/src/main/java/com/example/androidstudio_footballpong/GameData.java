1db67317fc1f4b7452054e8b867f5bd3cb357700
efeka <efekaraman@hotmail.com>
2021-04-09 14:11:00 +0300

    created GameData class for keeping track of game time and scores

---
+++ app/src/main/java/com/example/androidstudio_footballpong/GameData.java
@@ -0,0 +1,81 @@
+package com.example.androidstudio_footballpong;
+
+public class GameData {
+
+    public static final int DIFFICULTY_EASY = 0;
+    public static final int DIFFICULTY_MEDIUM = 1;
+    public static final int DIFFICULTY_HARD = 2;
+
+    private int score1 = 0, score2 = 0;
+    private int gameTimer = 60;
+    private int difficulty = DIFFICULTY_MEDIUM;
+,
+    public int decrementTimer(int seconds) {
+        gameTimer -= seconds;
+        return gameTimer >= 0 ? gameTimer : (gameTimer = 0);
+    }
+
+    public int getScore1() {
+        return score1;
+    }
+
+    public void setScore1(int score1) {
+        this.score1 = score1;
+    }
+
+    public int getScore2() {
+        return score2;
+    }
+
+    public void setScore2(int score2) {
+        this.score2 = score2;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getGameTimer() {
+        return gameTimer;
+    }
+
+    public void setGameTimer(int seconds) {
+        gameTimer = seconds;
+    }
+
+    public void setGameTimer(int minutes, int seconds) {
+        gameTimer = minutes * 60 + seconds;
+    }
+
+    public String getTimerDisplay() {
+        return convertTimerToString();
+    }
+
+    private String convertTimerToString() {
+        int temp = gameTimer;
+        int minutes = 0, seconds;
+        String timerDisplay;
+        while (temp >= 60) {
+            temp -= 60;
+            minutes++;
+        }
+        seconds = temp;
+
+        if (minutes < 10)
+            timerDisplay = "0" + minutes + ":";
+        else
+            timerDisplay = minutes + ":";
+
+        if (seconds < 10)
+            timerDisplay += "0" + seconds;
+        else
+            timerDisplay += seconds;
+
+        return timerDisplay;
+    }
+
+}
